683d265994c8932ba8f468aeda5104bf4c434d0d
pratibhak3110 <pratibhakamble3110@gmail.com>
2020-10-03 17:47:34 +0530

    new file added

--- src/app/reactiveform/reactiveform.component.ts
+++ src/app/reactiveform/reactiveform.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactiveform',
@@ -8,18 +8,50 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class ReactiveformComponent implements OnInit {
   signupForm: FormGroup;
+  invalidNamesArr:string[]=['hello','angular'];
   constructor() { }
 
   ngOnInit(): void {
     this.signupForm= new FormGroup({
-      name: new FormControl(null,[Validators.required,Validators.minLength(5)]),
-      email: new FormControl(null,[Validators.required,Validators.email]),
+      name: new FormControl(null,[
+            Validators.required,
+            Validators.minLength(5),
+            this.invalidNameValidation.bind(this),
+          ]),
+      email: new FormControl(null,[
+              Validators.required,
+              Validators.email
+            ]),
+      passwordgroup: new FormGroup({
+      password:new FormControl(null,[
+                Validators.required]),
+      confirmpassword: new FormControl(null,[
+                Validators.required]),
+              },
+              [this.matchPassword.bind(this)]),
+      gender:new FormControl("male"),
+
+      city: new FormControl("bengaluru",[Validators.required]),
     });
 
   }
 
   onSignup(){
     console.log(this.signupForm);
+  }
+
+  invalidNameValidation(control: AbstractControl):{[key:string]:boolean}{
+    if(this.invalidNamesArr.indexOf(control.value)>=0){
+      return {invalidName:true};
+    }
+    return null;
+  }
+
+  matchPassword(control: AbstractControl): {[key:string]:boolean}{
+    if(control.get('password').value!= control.get('confirmpassword').value){
+      return {passwordMatchError: true};
+    }
+    return null;
   }
 
 }
